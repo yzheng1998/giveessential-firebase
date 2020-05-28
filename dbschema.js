a78f1e97ff8b03b662eca77733cca257f40113f9
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2020-05-28 15:05:09 -0500

    add dbschema

---
+++ dbschema.js
@@ -0,0 +1,48 @@
+let db = {
+  donors: [
+    {
+      cell: "+19073988674",
+      city: "Anchorage",
+      donationMethod: "Ship from home, Visit post office, Online order",
+      email: "marcjohnson777@gmail.com",
+      items: "Kids' Activities (ex: electronics/board games/puzzles)",
+      matchNumber: "5",
+      name: "Marc Johnson",
+      notes:
+        "I have a lot of puzzles and games. I hope there are people in Anchorage, Alaska that would be interested. I could potentially share your website with people in Anchorage. My daughter used to go to Dartmouth and she shared your website with me. I also have a coworker who is a Dartmouth grad. Thanks for doing this!",
+      pledge: "TRUE",
+      row: "206",
+      state: "Alaska",
+      submittedAt: "5/6/2020 4:08:31",
+      token: "kru8mib590k6z0zk9189kru8mib5vof7",
+      zip: "99517",
+    },
+  ],
+  essentialWorkers: [
+    {
+      address: "13302 SE River RD Milwaukie OR 97222",
+      canShareWithDonor: "TRUE",
+      canShareWithMedia: "TRUE",
+      cell: "+19712239620",
+      city: "Milwaukie",
+      consent: "TRUE",
+      description:
+        "We are a struggling family with two small children student loan debt, high rent and a vehicle payment. I work multiple gig jobs often with our children for low pay to help make ends meet. We could use any help through this hard time for personal protective equipment to keep working these jobs serving the public. Anything would be greatly appreciated to get us through this difficult time for us all.",
+      email: "ardenpeacock@gmail.com",
+      firstName: "Arden",
+      items:
+        "Cleaning Supplies (disinfectant wipes/disposable gloves/dish soap), Masks (cloth/homemade, face shield, surgical/disposable), Gift Cards (ex: gas/groceries/restaurants), Kids' Activities (ex: electronics/board games/puzzles), Feminine Hygiene Products, Personal Hygiene Products (ex: soap/detergent/shampoo/lotion)",
+      jobTitle: "Independent contractor",
+      lastName: "Peacock-Gutierrez",
+      notes: "Really appreciative of this service in our time of need!",
+      proof:
+        "https://api.typeform.com/responses/files/660bf3d59a2209c1938914238a669d5d016ec41aeea7d2e0ec74f4e417b018b2/Screenshot_20200506_112641_Drive.jpg",
+      specifications:
+        "Kids age 6 & 3. Personal hygiene sz 6 diapers, wipes, laundry, dish soap, handsanitiser, gloves or masks. Gas or grocery gift cards.",
+      state: "Oregon",
+      submittedAt: "5/6/2020 18:42:42",
+      token: "xt1zfltfapu62ng39w0xt1zdetupneou",
+      zip: "97222",
+    },
+  ],
+};
